f2e463c96ecbdea75f26bf9a6b4f0a46f69238d7
Gabriel Valmarath <melovalmarath.gabriel@gmail.com>
2022-08-16 21:39:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('.p_s');
+let mElement = document.querySelector('.p_m');
+let hElement = document.querySelector('.p_h');
+
+function updtadeClock() {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`;
+
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60) * minute) - 90;
+    let hDeg = ((360/12) * hour) + ((30/60) * minute)- 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;
+    mElement.style.transform = `rotate(${mDeg}deg)`;
+    hElement.style.transform = `rotate(${hDeg}deg)`;
+}
+
+function fixZero(time) {
+    /*if(time < 10) {
+        return '0'+time;
+    }else {
+        return time;
+    }*/
+
+    return time < 10 ? `0${time}` : time;
+}
+
+setInterval(updtadeClock, 1000);
+updtadeClock(); //Roda a função imediatamente, caso contrário teria o delay de 1 segundo do setinterval
